25a1ed4f34bf5e840b5e348f1e0d0ac35f385ef6
timatrie <seb.steinig@googlemail.com>
2014-09-05 10:52:51 +0200

    minor changes

--- matlab/vwavelet_coherence_test.m
+++ matlab/vwavelet_coherence_test.m
@@ -42,22 +42,25 @@ min_scale_right=2;
 max_scale_right=400;
 
 line_width=2;
-line_style='--';
-line_color='k';
+line_style='-';
+line_color=[.7 .7 .7];
 font_size=16;
+arrow_size=.5;
+Arrow_Head_Size=.4;
+Arrow_Density=[20 20];
+
+colormap(ct);
 
 period_1=87;
 period_2=210;
 period_3=1000;
 
-%figure(1)
-%maximize(1)
-
-colormap(ct);
-
-cwt_data_left={roth,vieira,steinhilber09,steinhilber12};
-cwt_names_left={'Roth et al. (2012)','Vieira et al. (2011)','Steinhilber et al. (2009)','Steinhilber et al. (2012)'};
-
+% figure(1)
+% maximize(1)
+% 
+% cwt_data_left={roth,vieira,steinhilber09,steinhilber12};
+% cwt_names_left={'Roth et al. (2012)','Vieira et al. (2011)','Steinhilber et al. (2009)','Steinhilber et al. (2012)'};
+% 
 % % left panel side
 % for nn=1:4   
 %     subplot(4,2,(nn*2)-1)
@@ -92,36 +95,98 @@ cwt_names_left={'Roth et al. (2012)','Vieira et al. (2011)','Steinhilber et al. 
 %     set(h6,'linewidth',line_width,'linestyle',line_style,'color',line_color)  
 % end
 % 
-% export_fig('../../../plots/wavelets/cwt','-png','-opengl','-r100')
+% export_fig('../../plots/wavelets/cwt','-png','-opengl','-r100')
+% close(1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Cross wavelet transform (XWT)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% figure(1)
+% maximize(1)
+% 
+% xwt_data_left={vieira,steinhilber09,steinhilber12};
+% xwt_data_left_reference={roth_10,roth_5,roth_22};
+% xwt_names_left={'XWT between Roth and Vieira','XWT between Roth and Steinhilber (2009)','XWT between Roth and Steinhilber (2012)'};
+% 
+% for nn=1:3
+%     subplot(4,2,(nn*2)-1)
+%     xwt(cell2mat(xwt_data_left_reference(nn)),cell2mat(xwt_data_left(nn)),'S0',min_scale_left,'maxscale',max_scale_left,'Pad',1,'ArrowDensity',Arrow_Density,'ArrowSize',arrow_size,'ArrowHeadSize',Arrow_Head_Size)
+%     title(xwt_names_left(nn),'fontsize',font_size);
+%     ylabel('Period','fontsize',font_size)
+%     h1=line([-10000 2000],[log2(period_1) log2(period_1)],[0 0]);
+%     h2=line([-10000 2000],[log2(period_2) log2(period_2)],[0 0]); 
+%     h3=line([-10000 2000],[log2(period_3) log2(period_3)],[0 0]); 
+%     set(h1,'linewidth',line_width,'linestyle',line_style,'color',line_color)
+%     set(h2,'linewidth',line_width,'linestyle',line_style,'color',line_color)
+%     set(h3,'linewidth',line_width,'linestyle',line_style,'color',line_color) 
+% end
+% 
+% xwt_data_right={sbf,db,mea,vk};
+% xwt_data_right_reference={roth,roth,roth,roth};
+% xwt_names_right={'XWT between Roth and SBF','XWT between Roth and DB','XWT between Roth and MEA','XWT between Roth and VK'};
+% 
+% for nn=1:4
+%     subplot(4,2,nn*2)
+%     xwt(cell2mat(xwt_data_right_reference(nn)),cell2mat(xwt_data_right(nn)),'S0',min_scale_right,'maxscale',max_scale_right,'Pad',1,'ArrowDensity',Arrow_Density,'ArrowSize',arrow_size,'ArrowHeadSize',Arrow_Head_Size)
+%     title(xwt_names_right(nn),'fontsize',font_size);
+%     ylabel('Period','fontsize',font_size)
+%     h4=line([800 2000],[log2(period_1) log2(period_1)],[0 0]);
+%     h5=line([800 2000],[log2(period_2) log2(period_2)],[0 0]); 
+%     h6=line([800 2000],[log2(period_3) log2(period_3)],[0 0]); 
+%     set(h4,'linewidth',line_width,'linestyle',line_style,'color',line_color)
+%     set(h5,'linewidth',line_width,'linestyle',line_style,'color',line_color)
+%     set(h6,'linewidth',line_width,'linestyle',line_style,'color',line_color) 
+% end
+% 
+% export_fig('../../plots/wavelets/xwt','-png','-opengl','-r100')
 % close(1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Cross wavelet transform (XWT)
+%%Wavelet coherence (WTC)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-figure(2)
-maximize(2)
 
-xwt_data_left={vieira,steinhilber09,steinhilber12};
-xwt_data_left_reference={roth_10,roth_5,roth_22};
-xwt_names_left={'XWT between Roth and Vieira','XWT between Roth and Steinhilber (2009)','XWT between Roth and Steinhilber (2012)'};
+figure(1)
+maximize(1)
+
+wtc_data_left={vieira,steinhilber09,steinhilber12};
+wtc_data_left_reference={roth_10,roth_5,roth_22};
+wtc_names_left={'WTC between Roth and Vieira','WTC between Roth and Steinhilber (2009)','WTC between Roth and Steinhilber (2012)'};
 
 for nn=1:3
     subplot(4,2,(nn*2)-1)
-    xwt(cell2mat(xwt_data_left_reference(nn)),cell2mat(xwt_data_left(nn)),'S0',min_scale_left,'maxscale',max_scale_left,'Pad',1,'ArrowDensity',[20 20],'ArrowSize',.5,'ArrowHeadSize',.4)
-    title(xwt_names_left(nn),'fontsize',font_size);
+    wtc(cell2mat(wtc_data_left_reference(nn)),cell2mat(wtc_data_left(nn)),'S0',min_scale_left,'maxscale',max_scale_left,'Pad',1,'ArrowDensity',Arrow_Density,'ArrowSize',arrow_size,'ArrowHeadSize',Arrow_Head_Size)
+    title(wtc_names_left(nn),'fontsize',font_size);
+    set(gca,'fontsize',font_size);
+    ylabel('Period','fontsize',font_size)
+    h1=line([-10000 2000],[log2(period_1) log2(period_1)],[0 0]);
+    h2=line([-10000 2000],[log2(period_2) log2(period_2)],[0 0]); 
+    h3=line([-10000 2000],[log2(period_3) log2(period_3)],[0 0]); 
+    set(h1,'linewidth',line_width,'linestyle',line_style,'color',line_color)
+    set(h2,'linewidth',line_width,'linestyle',line_style,'color',line_color)
+    set(h3,'linewidth',line_width,'linestyle',line_style,'color',line_color)  
+
 end
 
-xwt_data_right={sbf,db,mea,vk};
-xwt_data_right_reference={roth,roth,roth,roth};
-xwt_names_right={'XWT between Roth and SBF','XWT between Roth and DB','XWT between Roth and MEA','XWT between Roth and VK'};
+wtc_data_right={sbf,db,mea,vk};
+wtc_data_right_reference={roth,roth,roth,roth};
+wtc_names_right={'WTC between Roth and SBF','WTC between Roth and DB','WTC between Roth and MEA','WTC between Roth and VK'};
 
 for nn=1:4
     subplot(4,2,nn*2)
-    xwt(cell2mat(xwt_data_right_reference(nn)),cell2mat(xwt_data_right(nn)),'S0',min_scale_right,'maxscale',max_scale_right,'Pad',1,'ArrowDensity',[20 20],'ArrowSize',.5,'ArrowHeadSize',.4)
-    title(xwt_names_right(nn),'fontsize',font_size);
+    wtc(cell2mat(wtc_data_right_reference(nn)),cell2mat(wtc_data_right(nn)),'S0',min_scale_right,'maxscale',max_scale_right,'Pad',1,'ArrowDensity',Arrow_Density,'ArrowSize',arrow_size,'ArrowHeadSize',Arrow_Head_Size)
+    title(wtc_names_right(nn),'fontsize',font_size);
+    set(gca,'fontsize',font_size);
+    ylabel('Period','fontsize',font_size)
+    h4=line([800 2000],[log2(period_1) log2(period_1)],[0 0]);
+    h5=line([800 2000],[log2(period_2) log2(period_2)],[0 0]); 
+    h6=line([800 2000],[log2(period_3) log2(period_3)],[0 0]); 
+    set(h4,'linewidth',line_width,'linestyle',line_style,'color',line_color)
+    set(h5,'linewidth',line_width,'linestyle',line_style,'color',line_color)
+    set(h6,'linewidth',line_width,'linestyle',line_style,'color',line_color)
 end
 
-export_fig('../../../plots/wavelets/xwt','-png','-opengl','-r100')
-close(2);
+%export_fig('../../plots/wavelets/wtc','-png','-opengl','-r200')
+%close;
     
